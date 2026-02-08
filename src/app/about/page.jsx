ab42af18628e1ce9b71270d02abfde281b9fbc0d
Qisth <qisthgames@gmail.com>
2026-02-08 15:10:51 +0700

    Update: about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,15 +1,9 @@
 "use server";
 
-import Counter from "@/components/Counter";
-
 export default async function AboutPage() {
-  // const res = await fetch("/api/halo");
-  // const data = await res.json();
-  // console.log("Data dari API Route:", data);
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Counter />
+      <span className="text-2xl font-semibold">Ini adalah halaman about</span>
     </div>
   );
 }
