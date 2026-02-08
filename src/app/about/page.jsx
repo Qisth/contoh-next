048c3e6bf1b1100e6ae726cba7f88d4f9c4fc595
Qisth <qisthgames@gmail.com>
2026-02-08 15:04:02 +0700

    fix: build

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,9 @@
 import Counter from "@/components/Counter";
 
 export default async function AboutPage() {
-  const res = await fetch("https://localhost:3000/api/halo");
-  const data = await res.json();
-  console.log("Data dari API Route:", data);
+  // const res = await fetch("/api/halo");
+  // const data = await res.json();
+  // console.log("Data dari API Route:", data);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
